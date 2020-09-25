ff23defa1c2d9610fe9bad5953fa0154bc5d0470
zhuzhiqiang <15003873032@163.com>
2020-09-25 17:59:36 +0800

    小程序分类增加

--- 微信小程序/UG/miniprogram/pages/catrgory/index.js
+++ 微信小程序/UG/miniprogram/pages/catrgory/index.js
@@ -1,66 +1,27 @@
 // pages/catrgory/index.js
+import {request} from "../../request/index.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    categorieList:"",
+    currentIndex:0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getCatrgoryFn()
   },
+    getCatrgoryFn(){
+      request({url:"https://api-hmugo-web.itheima.net/api/public/v1/categories"}).then(res=>{
+        console.log('atrgory',res.data.message);
+        this.setData({
+          categorieList:res.data.message
+        })
+      })
+    },
+    handleItemTap(e){
+      console.log(e.target.dataset.index)
+      let currentIndex = e.target.dataset.index
+      this.setData({
+        currentIndex
+      })
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
