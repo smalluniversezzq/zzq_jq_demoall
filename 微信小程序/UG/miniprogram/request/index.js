d21c376e2add8e1362c27d8a3d7bee52d4b7febe
zhuzhiqiang <15003873032@163.com>
2020-10-06 18:04:56 +0800

    增加ajax loading

--- 微信小程序/UG/miniprogram/request/index.js
+++ 微信小程序/UG/miniprogram/request/index.js
@@ -1,5 +1,11 @@
+let axajTimes = 0;
 export const request = (params)=>{
   // console.log(...params,'params')
+    axajTimes++;
+    wx.showLoading({
+      title:"加载中",
+      mask:true
+    })
   const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1"
   return new Promise((resolve,reject)=>{
     wx.request({
@@ -12,6 +18,10 @@ export const request = (params)=>{
       fail:(err)=>{
         reject(err)
       },
+      complete:()=>{
+        axajTimes--;
+        axajTimes===0?wx.hideLoading():""
+      }
     })
   })
 }
